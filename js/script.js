a23036979fd3ef40c06fb7133dd38ad327618a4d
Dmitry <Noxious22@mail.ru>
2020-03-21 17:30:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+   $('.main__slider').slick({
+      arrows: true,
+      dots: false
+   });
+});
+
+$(document).ready(function () {
+   $('.second__second-slider').slick({
+      arrows: true,
+      dots: false
+   });
+});
