87e27079fce87fd2b35f08051a9fb8571923a5d2
Dmitry <Noxious22@mail.ru>
2020-03-23 23:41:48 +0300

    pixel perfect

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ $(document).ready(function () {
       dots: false
    });
 });
+
+$(document).ready(function () {
+   $('.clients__slider').slick({
+      arrows: true,
+      dots: false
+   });
+});
+
+$(document).ready(function () {
+   $('.clients-logo__slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false
+   });
+});
+
+$(document).ready(function () {
+   $('.responsive__slider').slick({
+      arrows: true,
+      dots: false
+   });
+});
