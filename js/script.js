09ee69e59212715033cf857e10729023683fa79e
Dmitry <Noxious22@mail.ru>
2020-03-24 22:45:39 +0300

    without clients-logo

--- js/script.js
+++ js/script.js
@@ -34,3 +34,18 @@ $(document).ready(function () {
       dots: false
    });
 });
+
+let acc = document.getElementsByClassName("footer__accordeon-btn");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+   acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+         panel.style.display = "none";
+      } else {
+         panel.style.display = "block";
+      }
+   });
+}
